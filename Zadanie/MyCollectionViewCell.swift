a2c6d464a50c49a3d884886d95017d4934fef4d5
Adrian <adrian.kubala@nextisgreat.com>
2016-08-08 13:07:10 +0200

    Added UICollectionView object with several UIImage objects within in.

---
+++ Zadanie/MyCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyCollectionViewCell.swift
+//  Zadanie
+//
+//  Created by Adrian on 08.08.2016.
+//  Copyright © 2016 Adrian. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var myImage: UIImageView!
+    
+}
