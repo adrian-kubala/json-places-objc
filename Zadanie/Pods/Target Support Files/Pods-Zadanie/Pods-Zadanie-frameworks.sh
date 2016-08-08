91da230f63dee66a96392c1256f93c864eb17412
Adrian <adrian.kubala@nextisgreat.com>
2016-08-08 14:14:23 +0200

    Added Decodable library

--- Zadanie/Pods/Target Support Files/Pods-Zadanie/Pods-Zadanie-frameworks.sh
+++ Zadanie/Pods/Target Support Files/Pods-Zadanie/Pods-Zadanie-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Decodable/Decodable.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Decodable/Decodable.framework"
 fi
