d0b7c54f8b0392fb44bb6fdfdb7ecd6065871818
Adrian <adrian.kubala@nextisgreat.com>
2016-10-07 15:22:02 +0200

    Changed methods' order

--- Zadanie/Zadanie/JsonTableViewController.swift
+++ Zadanie/Zadanie/JsonTableViewController.swift
@@ -13,183 +13,174 @@ import AlamofireImage
 import CoreLocation
 
 class JsonTableViewController: UITableViewController {
+  
+  let reuseIdentifier = "cell"
+  let segueIdentifier = "placesSegue"
+  
+  var cachedImages = [Int : UIImage?]()
+  var cachedImagesToPass = [UIImage?]()
+  
+  var places = [Place]()
+  var placesToPass = [Place]()
+  
+  var distancesToPass = [Double]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    let reuseIdentifier = "cell"
-    let segueIdentifier = "placesSegue"
-    
-    var cachedImages = [Int : UIImage?]()
-    var cachedImagesToPass = [UIImage?]()
-    
-    var places = [Place]()
-    var placesToPass = [Place]()
-    
-    var distancesToPass = [Double]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let jsonUrl = "https://dl.dropboxusercontent.com/u/18389601/development/test/Location/locations.json"
-        fetchJson(jsonUrl)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return places.count ?? 0
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cellRow = indexPath.row
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath)
-        
-        fillCell(cellRow, passedCell: cell)
-        
-        return cell
-    }
-    
-    func fillCell(row: Int, passedCell: UITableViewCell) {
-        guard let myCell = passedCell as? MyTableViewCell else {
-            return
+    let jsonUrl = "https://dl.dropboxusercontent.com/u/18389601/development/test/Location/locations.json"
+    fetchJson(jsonUrl)
+  }
+  
+  func fetchJson(url: String) {
+    Alamofire.request(.GET, url).validate().responseJSON { response in
+      switch response.result {
+      case .Success:
+        if let value = response.result.value {
+          self.fillPlacesWithJson(JSON(value))
+          self.tableView.reloadData()
         }
+      case .Failure(let error):
+        print(error)
+      }
+    }
+  }
+  
+  func fillPlacesWithJson(json:JSON?) {
+    
+    for object in (json?.array)! {
+      let name = object["name"].stringValue
+      let image = object["pin_url"].stringValue
+      let lat = object["coordinate"]["latitude"].doubleValue
+      let lon = object["coordinate"]["longitude"].doubleValue
+      
+      let place = Place(pinImageUrl: image, pinName: name, latitude: lat, longitude: lon)
+      places.append(place)
+    }
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return places.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cellRow = indexPath.row
+    
+    let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath)
+    
+    fillCell(cellRow, passedCell: cell)
+    
+    return cell
+  }
+  
+  func fillCell(row: Int, passedCell: UITableViewCell) {
+    guard let myCell = passedCell as? MyTableViewCell else {
+      return
+    }
+    
+    myCell.labelName.text = places[row].pinName
+    
+    let pinUrl = places[row].pinImageUrl
+    
+    guard let cachedImage = cachedImages[row] else {
+      getPin(pinUrl, completion: { (image) in
         
-        myCell.labelName.text = places[row].pinName
-        
-        let pinUrl = places[row].pinImageUrl
-        
-        guard let cachedImage = cachedImages[row] else {
-            getPin(pinUrl, completion: { (image) in
-                
-                self.resizeImage(image!, newWidth: 30) { (scaledImage) in
-                    self.cachedImages[row] = scaledImage
-                    myCell.pinImage.image = self.cachedImages[row]!
-                }
-            })
-            return
+        self.resizeImage(image!, newWidth: 30) { (scaledImage) in
+          self.cachedImages[row] = scaledImage
+          myCell.pinImage.image = self.cachedImages[row]!
         }
-        
-        myCell.pinImage.image = cachedImage
+      })
+      return
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        guard segue.identifier == segueIdentifier else {
-            return
-        }
-        
-        guard let destinationViewController = segue.destinationViewController as? PlacesTableVC else {
-            return
-        }
-        
-        destinationViewController.passedPlaces = placesToPass
-        destinationViewController.passedCachedImages = cachedImagesToPass
-        destinationViewController.distances = distancesToPass
+    myCell.pinImage.image = cachedImage
+  }
+  
+  func getPin(url: String, completion: (UIImage?) -> ()) {
+    Alamofire.request(.GET, url).response() {
+      (_, _, data, _) in
+      let image = UIImage(data: data! )
+      completion(image)
+    }
+  }
+  
+  func resizeImage(image: UIImage, newWidth: CGFloat, completion: (scaledImage: UIImage) -> ()) {
+    
+    let qualityOfServiceClass = QOS_CLASS_BACKGROUND
+    let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
+    dispatch_async(backgroundQueue, {
+      // This is run on the background queue
+      
+      let scale = newWidth / image.size.width
+      let newHeight = image.size.height * scale
+      UIGraphicsBeginImageContext(CGSizeMake(newWidth, newHeight))
+      image.drawInRect(CGRectMake(0, 0, newWidth, newHeight))
+      let newImage = UIGraphicsGetImageFromCurrentImageContext()
+      UIGraphicsEndImageContext()
+      
+      dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        // This is run on the main queue, after the previous code in outer block
+        completion(scaledImage: newImage)
+      })
+    })
+  }
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    let selectedCell = indexPath.row
+    
+    placesToPass = []
+    cachedImagesToPass = []
+    distancesToPass = []
+    
+    let latitude = places[selectedCell].latitude
+    let longitude = places[selectedCell].longitude
+    let coordinates = (latitude, longitude)
+    
+    getAllDistances(coordinates, cellRow: selectedCell)
+    
+    performSegueWithIdentifier(segueIdentifier, sender: self)
+  }
+  
+  func getAllDistances(placeCoordinates: (Double, Double), cellRow: Int) {
+    for (i, _) in places.enumerate() {
+      guard i != cellRow else {
+        continue
+      }
+      
+      let otherLatitude = places[i].latitude
+      let otherLongitude = places[i].longitude
+      let otherCoordiantes = (otherLatitude, otherLongitude)
+      
+      let distance = getDistanceInKm(placeCoordinates, from: otherCoordiantes)
+      
+      guard distance <= 2 else {
+        continue
+      }
+      
+      placesToPass.append(places[i])
+      cachedImagesToPass.append(cachedImages[i]!)
+      distancesToPass.append(distance)
+    }
+  }
+  
+  func getDistanceInKm(place: (Double, Double), from otherPlace: (Double, Double)) -> Double {
+    let firstLocation = CLLocation(latitude: place.0, longitude: place.1)
+    let seceondLocation = CLLocation(latitude: otherPlace.0, longitude: otherPlace.1)
+    
+    return firstLocation.distanceFromLocation(seceondLocation) / 1000
+  }
+  
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    guard segue.identifier == segueIdentifier else {
+      return
     }
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let selectedCell = indexPath.row
-        
-        placesToPass = []
-        cachedImagesToPass = []
-        distancesToPass = []
-        
-        let latitude = places[selectedCell].latitude
-        let longitude = places[selectedCell].longitude
-        let coordinates = (latitude, longitude)
-        
-        getAllDistances(coordinates, cellRow: selectedCell)
-        
-        performSegueWithIdentifier(segueIdentifier, sender: self)
+    guard let destinationViewController = segue.destinationViewController as? PlacesTableVC else {
+      return
     }
     
-    func getAllDistances(placeCoordinates: (Double, Double), cellRow: Int) {
-        for (i, _) in places.enumerate() {
-            guard i != cellRow else {
-                continue
-            }
-            
-            let otherLatitude = places[i].latitude
-            let otherLongitude = places[i].longitude
-            let otherCoordiantes = (otherLatitude, otherLongitude)
-            
-            let distance = getDistanceInKm(placeCoordinates, from: otherCoordiantes)
-            
-            guard distance <= 2 else {
-                continue
-            }
-            
-            placesToPass.append(places[i])
-            cachedImagesToPass.append(cachedImages[i]!)
-            distancesToPass.append(distance)
-        }
-    }
-    
-    func getDistanceInKm(place: (Double, Double), from otherPlace: (Double, Double)) -> Double {
-        let firstLocation = CLLocation(latitude: place.0, longitude: place.1)
-        let seceondLocation = CLLocation(latitude: otherPlace.0, longitude: otherPlace.1)
-        
-        return firstLocation.distanceFromLocation(seceondLocation) / 1000
-    }
-    
-    func resizeImage(image: UIImage, newWidth: CGFloat, completion: (scaledImage: UIImage) -> ()) {
-        
-        let qualityOfServiceClass = QOS_CLASS_BACKGROUND
-        let backgroundQueue = dispatch_get_global_queue(qualityOfServiceClass, 0)
-        dispatch_async(backgroundQueue, {
-            // This is run on the background queue
-            
-            let scale = newWidth / image.size.width
-            let newHeight = image.size.height * scale
-            UIGraphicsBeginImageContext(CGSizeMake(newWidth, newHeight))
-            image.drawInRect(CGRectMake(0, 0, newWidth, newHeight))
-            let newImage = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                // This is run on the main queue, after the previous code in outer block
-                completion(scaledImage: newImage)
-            })
-        })
-    }
-    
-    func fetchJson(url: String) {
-        Alamofire.request(.GET, url).validate().responseJSON { response in
-            switch response.result {
-            case .Success:
-                if let value = response.result.value {
-                    self.fillPlacesWithJson(JSON(value))
-                    self.tableView.reloadData()
-                }
-            case .Failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func fillPlacesWithJson(json:JSON?) {
-        
-        for object in (json?.array)! {
-            let name = object["name"].stringValue
-            let image = object["pin_url"].stringValue
-            let lat = object["coordinate"]["latitude"].doubleValue
-            let lon = object["coordinate"]["longitude"].doubleValue
-            
-            let place = Place(pinImageUrl: image, pinName: name, latitude: lat, longitude: lon)
-            places.append(place)
-        }
-    }
-    
-    func getPin(url: String, completion: (UIImage?) -> ()) {
-        Alamofire.request(.GET, url).response() {
-            (_, _, data, _) in
-            let image = UIImage(data: data! )
-            completion(image)
-        }
-    }
+    destinationViewController.passedPlaces = placesToPass
+    destinationViewController.passedCachedImages = cachedImagesToPass
+    destinationViewController.distances = distancesToPass
+  }
 }
--- Zadanie/Zadanie/PlacesTableVC.swift
+++ Zadanie/Zadanie/PlacesTableVC.swift
@@ -9,50 +9,37 @@
 import UIKit
 
 class PlacesTableVC: UITableViewController {
-    var passedPlaces = [Place]()
-    var passedCachedImages = [UIImage?]()
-    var distances = [Double]()
+  var passedPlaces = [Place]()
+  var passedCachedImages = [UIImage?]()
+  var distances = [Double]()
+  
+  var reuseIdentifier = "placesTableCell"
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return passedPlaces.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath)
     
-    var reuseIdentifier = "placesTableCell"
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return passedPlaces.count ?? 0
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath)
-        
-        let cellIndex = indexPath.row
-        
-        if let myCell = cell as? MyTableViewCell {
-            
-            myCell.placeImage.image = passedCachedImages[cellIndex]!
-            myCell.placeLabel.text = passedPlaces[cellIndex].pinName
-        }
-        
-        return cell
+    let cellIndex = indexPath.row
+    
+    if let myCell = cell as? MyTableViewCell {
+      
+      myCell.placeImage.image = passedCachedImages[cellIndex]!
+      myCell.placeLabel.text = passedPlaces[cellIndex].pinName
     }
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let latitude = passedPlaces[indexPath.row].latitude
-        let longitude = passedPlaces[indexPath.row].longitude
-        let distance = distances[indexPath.row]
-        
-        let alert = UIAlertController(title: "Współrzędne", message: "Szerokość: \(latitude)\nDługość: \(longitude)\nOdległość: \(String(format:"%.2f", distance)) km", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
+    return cell
+  }
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    let latitude = passedPlaces[indexPath.row].latitude
+    let longitude = passedPlaces[indexPath.row].longitude
+    let distance = distances[indexPath.row]
+    
+    let alert = UIAlertController(title: "Współrzędne", message: "Szerokość: \(latitude)\nDługość: \(longitude)\nOdległość: \(String(format:"%.2f", distance)) km", preferredStyle: UIAlertControllerStyle.Alert)
+    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+    self.presentViewController(alert, animated: true, completion: nil)
+  }
 }
