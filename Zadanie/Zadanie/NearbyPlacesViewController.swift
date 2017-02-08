9b3cfd05a74decc925f2d01f77af629bb9bd5619
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-08 23:32:13 +0100

    Remove unnecessary property

--- Zadanie/Zadanie/NearbyPlacesViewController.swift
+++ Zadanie/Zadanie/NearbyPlacesViewController.swift
@@ -11,14 +11,12 @@ import UIKit
 class NearbyPlacesViewController: UITableViewController {
   var nearbyPlaces: [Place] = []
   
-  var reuseIdentifier = "placesTableCell"
-  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return nearbyPlaces.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! PlaceView
+    let cell = tableView.dequeueReusableCell(withIdentifier: "NearbyPlaceView", for: indexPath) as! PlaceView
     
     cell.pinImageView.image = nearbyPlaces[indexPath.row].pinImage
     cell.label.text = nearbyPlaces[indexPath.row].name
@@ -27,14 +25,15 @@ class NearbyPlacesViewController: UITableViewController {
   }
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    
     let location = nearbyPlaces[indexPath.row].location
     let latitude = location.coordinate.latitude
     let longitude = location.coordinate.longitude
     let distance = nearbyPlaces[indexPath.row].distance!
     
-    let alert = UIAlertController(title: "Współrzędne", message: "Szerokość: \(latitude)\nDługość: \(longitude)\nOdległość: \(String(format:"%.2f", distance)) km", preferredStyle: UIAlertControllerStyle.alert)
+    let alert = UIAlertController(title: "Współrzędne",
+                                  message: "Szerokość: \(latitude)\nDługość: \(longitude)\nOdległość: \(String(format:"%.2f", distance)) km",
+                                  preferredStyle: UIAlertControllerStyle.alert)
     alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-    self.present(alert, animated: true, completion: nil)
+    present(alert, animated: true, completion: nil)
   }
 }
