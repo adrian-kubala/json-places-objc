6c3d07831d317036c4a0907cd8faa3d784f631c0
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-08 23:48:00 +0100

    Use Place instance in PlaceViewController

--- Zadanie/Zadanie/PlaceViewController.swift
+++ Zadanie/Zadanie/PlaceViewController.swift
@@ -10,22 +10,22 @@ import UIKit
 import GoogleMaps
 
 class PlaceViewController: UIViewController {
-  var mapLatitude: Double!
-  var mapLongitude: Double!
-  var mapPinImage: UIImage!
-  var mapPinName: String!
+  var place: Place!
 
   override func loadView() {
-    let camera = GMSCameraPosition.camera(withLatitude: mapLatitude, longitude: mapLongitude, zoom: 16.0)
+    let latitude = place.location.coordinate.latitude
+    let longitude = place.location.coordinate.longitude
+    
+    let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: 16.0)
     let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
     mapView.isMyLocationEnabled = true
     view = mapView
     
     let marker = GMSMarker()
-    marker.position = CLLocationCoordinate2D(latitude: mapLatitude, longitude: mapLongitude)
+    marker.position = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     
-    marker.title = mapPinName
-    marker.icon = mapPinImage
+    marker.title = place.name
+    marker.icon = place.pinImage
     marker.map = mapView
     mapView.selectedMarker = marker
   }
