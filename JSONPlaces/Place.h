b3f2f700d88ba1254b6bf74587f35a553f579cae
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-10 19:29:25 +0100

    Declare class Place

---
+++ JSONPlaces/Place.h
@@ -0,0 +1,19 @@
+//
+//  Place.h
+//  JSONPlaces
+//
+//  Created by Adrian Kubała on 10.02.2017.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface Place : NSObject
+
+@property (nonatomic) NSString *name;
+@property (nonatomic) CLLocation *location;
+@property (nonatomic) UIImage *pinImage;
+@property (nonatomic) double distance;
+
+@end
---
+++ JSONPlaces/Place.m
@@ -0,0 +1,13 @@
+//
+//  Place.m
+//  JSONPlaces
+//
+//  Created by Adrian Kubała on 10.02.2017.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+#import "Place.h"
+
+@implementation Place
+
+@end
