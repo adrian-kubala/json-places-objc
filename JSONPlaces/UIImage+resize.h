92327b3c251f9504d54912ee163eaee43caf6b3b
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-12 00:52:41 +0100

    Declare header file

---
+++ JSONPlaces/UIImage+resize.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+resize.h
+//  JSONPlaces
+//
+//  Created by Adrian Kubała on 12.02.2017.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (resizeImage)
+
+- (void)resizeImage:(CGFloat)newWidth completion:(void (^)(UIImage *))completion;
+
+@end
