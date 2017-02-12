6b818d13457835c473917736104bb4319bd9f48c
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-12 15:44:04 +0100

    Declare category

---
+++ JSONPlaces/UITableViewController+gesture.h
@@ -0,0 +1,15 @@
+//
+//  UITableViewController+gesture.h
+//  JSONPlaces
+//
+//  Created by Adrian Kubała on 12.02.2017.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableViewController (gesture)
+
+- (void) addLongPressGestureRecognizer:(SEL)selector;
+
+@end
---
+++ JSONPlaces/UITableViewController+gesture.m
@@ -0,0 +1,13 @@
+//
+//  UITableViewController+gesture.m
+//  JSONPlaces
+//
+//  Created by Adrian Kubała on 12.02.2017.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+#import "UITableViewController+gesture.h"
+
+@implementation UITableViewController (gesture)
+
+@end
