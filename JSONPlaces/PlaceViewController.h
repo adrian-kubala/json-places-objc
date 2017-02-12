b51dead272d44c65515b403bbb9483838b32b959
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-12 20:32:53 +0100

    Declare class PlaceViewController

---
+++ JSONPlaces/PlaceViewController.h
@@ -0,0 +1,16 @@
+//
+//  PlaceViewController.h
+//  JSONPlaces
+//
+//  Created by Adrian Kubała on 12.02.2017.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Place.h"
+
+@interface PlaceViewController : UIViewController
+
+@property (nonatomic) Place *place;
+
+@end
