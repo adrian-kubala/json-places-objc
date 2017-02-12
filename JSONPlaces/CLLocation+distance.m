93d2c69a674545d19ad6d35fb041a88fa4fcb749
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-12 01:32:02 +0100

    Declare category

---
+++ JSONPlaces/CLLocation+distance.m
@@ -0,0 +1,13 @@
+//
+//  CLLocation+distance.m
+//  JSONPlaces
+//
+//  Created by Adrian Kubała on 12.02.2017.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+#import "CLLocation+distance.h"
+
+@implementation CLLocation (distance)
+
+@end
