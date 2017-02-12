56faccf3800e4ef64b17a6de612dcffa9037cb36
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-12 20:53:43 +0100

    Declare class

---
+++ JSONPlaces/NearbyPlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  NearbyPlacesViewController.h
+//  JSONPlaces
+//
+//  Created by Adrian Kubała on 12.02.2017.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NearbyPlacesViewController : UIViewController
+
+@end
---
+++ JSONPlaces/NearbyPlacesViewController.m
@@ -0,0 +1,23 @@
+//
+//  NearbyPlacesViewController.m
+//  JSONPlaces
+//
+//  Created by Adrian Kubała on 12.02.2017.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+#import "NearbyPlacesViewController.h"
+
+@interface NearbyPlacesViewController ()
+
+@end
+
+@implementation NearbyPlacesViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  
+}
+
+@end
