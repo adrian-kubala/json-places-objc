dc479b2435fc7ece43922d6d423af9b26b0b82bb
Adrian Kubała <adrian.kubala@gmail.com>
2017-02-13 14:19:50 +0100

    Implement fillPlacesWithJSON method

--- JSONPlaces/PlacesViewController.m
+++ JSONPlaces/PlacesViewController.m
@@ -8,6 +8,9 @@
 
 #import "PlacesViewController.h"
 #import "JSONParser.h"
+#import "NSURL+getImage.h"
+#import "UIImage+resize.h"
+#import "Place.h"
 
 @interface PlacesViewController ()
 
@@ -28,9 +31,30 @@
     }
     
     if (result) {
-//      self fill
+      [self fillPlacesWithJSON:result];
     }
   }];
+}
+
+- (void)fillPlacesWithJSON:(NSArray *)jsonArray {
+  for (NSDictionary *object in jsonArray) {
+    NSString *name = object[@"name"];
+    NSURL *imageURL = [[NSURL alloc] initWithString:object[@"pin_url"]];
+    
+    NSDictionary *coordinate = [[NSDictionary alloc] initWithDictionary:object[@"coordinate"]];
+    double lat = [[coordinate objectForKey:@"latitude"] doubleValue];
+    double lon = [[coordinate objectForKey:@"longitude"] doubleValue];
+    
+    [imageURL getImage:^(UIImage * image) {
+      [image resizeWithNewWidth:30 completion:^(UIImage * scaledImage) {
+        
+        Place *place = [[Place alloc] initWithName:name latitude:lat longitude:lon pinImage:scaledImage];
+        [self.places addObject:place];
+        
+        [self.tableView reloadData];
+      }];
+    }];
+  }
 }
 
 #pragma mark - Table view data source
